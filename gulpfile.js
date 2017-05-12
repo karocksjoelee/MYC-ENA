aa7297e8a0ce9e0b4b94236a160d8f122ba1ba50
JoeLee <karocksjoelee@gmail.com>
2017-05-12 18:18:36 +0800

    [NODE] Better logging for development .
    < npm install --save chalk figlet >

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,23 @@ const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
+const cm = require('./utility/common-module');
 
 gulp.task('default', ['serve','jshint', 'nodemon', 'watch-front-end','watch-back-end']);
 
+
+gulp.task('serve', () => {
+    browserSync.init({
+        proxy: {
+            target: 'localhost:7500',
+            ws: true
+        }
+    });
+});
+
+
 gulp.task('jshint', () => {
-    return gulp.src(['routes/**/*.js', 'middleware.js', 'bin/www'])
+    return gulp.src(['routes/**/*.js', 'middleware.js', 'bin/www.js'])
         .pipe(jshint({
             esnext: true
         }))
@@ -16,14 +28,14 @@ gulp.task('jshint', () => {
 
 gulp.task('watch-front-end', function () {
 
-    gulp.watch(['dist/*.html', 'dist/*.js']).on('change', browserSync.reload);
+    gulp.watch(['dist/*.html', 'dist/*.js', 'dist/*.*']).on('change', browserSync.reload);
 
 });
 
 gulp.task('watch-back-end', () => {
 
-    gulp.watch(['routes/**/*.js', 'server.js', 'bin/www', 'gulpfile.js']).on('change', () => {
-        console.log('[SERVER] File Changed');
+    gulp.watch(['routes/**/*.js', 'middleware.js', 'bin/www.js', 'gulpfile.js','dist/*.*']).on('change', () => {
+        cm.logWarn('[SERVER] Back-End File Changed');
     });
 
 });
@@ -34,15 +46,7 @@ gulp.task('nodemon',() => {
        env: {'NODE_ENV':'development'},
        ignore: ['src']
    }).on('restart',() => {
-       console.log('[SERVER] Restarted ');
+       cm.logWarn('[SERVER] Restarted ');
    });
 });
 
-gulp.task('serve', () => {
-    browserSync.init({
-        proxy: {
-            target: 'localhost:7500',
-            ws: true
-        }
-    });
-});
