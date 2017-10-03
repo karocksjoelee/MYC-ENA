a9069a2b201c1ddb7a25cb97bd7ec29d57fc03ef
JoeLee <karocksjoelee@gmail.com>
2017-10-03 23:20:17 +0800

    [NODE] Common Module - Error Helper

--- utility/common-module.js
+++ utility/common-module.js
@@ -6,16 +6,27 @@ const warn = chalk.yellow.bold;
 
 exports.logErr = function (errorMessage) {
     console.log(err(errorMessage));
-};
+}
 
 exports.logSuc = function (succMessage) {
     console.log(suc(succMessage));
-};
+}
 
 exports.logWarn = function (warnMessage) {
     console.log(warn(warnMessage));
-};
+}
 
-exports.logDev = function (logMessage) {
-    console.log(dev(devMesssage));
-};
+exports.logDev = function (devMessage) {
+    console.log(dev(devMessage));
+}
+
+exports.errHelper = function(err,next) {
+
+  this.logErr(`[MONGO] Operation Error :`);
+  console.log(err);
+  this.logErr(`------------------------------------------------------------------------------------`);
+  if (next) {
+    next();
+  }
+
+}
